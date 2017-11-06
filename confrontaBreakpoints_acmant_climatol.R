bcc0fe202568226d7bbe18120844397560601f3c
guidofioravanti <guidofioravanti@yahoo.it>
2017-11-06 12:35:41 +0100

    Programma per identificare i breakpoints comuni di ACMANT e Climatol

---
+++ confrontaBreakpoints_acmant_climatol.R
@@ -0,0 +1,88 @@
+#6 novembre 2017: programma che mette insieme i breakpoints di ACMANT con i breakpoints di Climatol
+#I breakpoints sono quelli trovati a livello mensile.
+
+#Il programma usa la funzione reformatACMANT() per leggere i breakpoints di ACMANT e trasporli in un dataframe.
+#Il programma inoltre si aspetta un corrispettivo file di breakpoints con i risultati di climatol. Questo file
+#viene identificato dal fatto di iniziare con il prefisso "climatol_". Se uno dei due file non viene trovato il programma termina.
+
+#Output: un dataframe "breakpoints_comuni_climatol_acmant.csv"
+#Due grafici: uno che illustra il numero di breakpoints per ciascuna serie, l'altro che mostra il numero di anni (in valore assoluto)
+#che intercorrono tra un breakpoint individuato da ACMANT e uno individuato da Climatol.
+rm(list=objects())
+library("tidyverse")
+library("cairoDevice")
+library("RColorBrewer")
+source("reformat_acmant_breakpoints.R")
+
+#tolleranza, gap, tra il breakpoint trovato da climatol e acmant
+GAP_ANNI<-2
+
+#i breakpoint vicini agli estremi della serie (anno di inizio o anno di fine) vengono scartati se distatno da anno inizio/fine meno di GAP_ANNI_ESTREMI
+GAP_ANNI_ESTREMI<-3
+
+#legge il file dei breakpoints di acmant e restituisce un df con i risultati per ciascuna serie (solo per le serie con 1 o più bp)
+reformatACMANT() %>%
+  mutate(Date=as.Date(Date)) %>%
+  rename(acmant_date=Date)->acmantBrk
+
+#cerca un file per Climatol.. se non lo trova il programma termina
+list.files(pattern = "^climatol.+csv$")->fileClimatol
+if(length(fileClimatol)!=1) stop(sprintf("Mi aspettavo un file per climatol ne ho trovati %d",length(fileClimatol)))
+read_delim(fileClimatol,delim=",",col_names = TRUE) %>%
+  mutate(metodo="Climatol") %>%
+  mutate(Date=as.Date(Date)) %>%
+  rename(climatol_date=Date)->climatolBrk
+
+full_join(climatolBrk,acmantBrk,by=c("Code"="Code")) %>% mutate(gapYears=round(as.double((climatol_date-acmant_date)/365),2) )->listaBrk
+  
+#calcoliamo la differenza (in termini di anni) tra il breakpoint trovato da ACMANT  e quello trovato da CLIMATOL:
+#il focus è sui breakpoint che distano in valore assoluto + o - GAP_ANNI
+listaBrk %>% 
+  filter(abs(gapYears)<=GAP_ANNI)->risultato1
+
+  risultato1$firstYear<-as.Date(paste0(unlist(map(str_split(risultato1$period,"-"),1)),"-01-01"))
+  risultato1$lastYear<-as.Date(paste0(unlist(map(str_split(risultato1$period,"-"),2)),"-12-31"))
+
+#a questo punto abbiamo solo i breakpoint vicini tra acmant e climatol: abbiamo però bisogno di un altro filtro
+#Ovvero: dobbiamo eliminare i breakpoint troppo vicini al punto di partenza della serie (firstYear o lastYear)
+
+risultato1 %>% mutate(zz=min(abs(as.double((climatol_date-firstYear)/365)),abs(as.double((acmant_date-firstYear)/365)))  )
+
+risultato1 %>% 
+  mutate(tmp1=abs(as.double((climatol_date-firstYear)/365)),tmp2=abs(as.double((acmant_date-firstYear)/365)),minimo1=ifelse(tmp1<=tmp2,tmp1,tmp2)) %>%
+  mutate(tmp3=abs(as.double((climatol_date-lastYear)/365)),tmp4=abs(as.double((acmant_date-lastYear)/365)),minimo2=ifelse(tmp3<=tmp4,tmp3,tmp4)) %>%
+  mutate(gap_from_extremes=ifelse(minimo1<=minimo2,minimo1,minimo2)) %>%
+  filter(gap_from_extremes>GAP_ANNI_ESTREMI) %>%
+  select(-matches("^tmp[0-9]"),-matches("^minimo[0-9]"))->risultato2
+
+write_delim(risultato2,"breakpoints_comuni_climatol_acmant.csv",delim=";",col_names=TRUE)
+
+rm(risultato1)
+
+risultato2 %<>%mutate(regione=str_replace(Code,"_.+",""),Code2=str_replace(Code,"^[^_]+_",""))
+cairo_pdf("conteggio_breakpoints.pdf",width=12,height=8,pointsize = 5,onefile = TRUE)
+purrr::walk(unique(risultato2$regione),.f=function(rr){
+  
+  risultato2 %>%
+  filter(regione==rr) %>%  
+  ggplot(aes(x=Code2))+
+  geom_bar(aes(fill=regione))+
+  facet_wrap(~regione,scales = "free_x")+
+    scale_fill_discrete(guide="none")+
+  theme_bw()+
+  theme(text=element_text(family="Lato"),
+        panel.grid.major = element_blank(),
+        strip.background = element_rect(fill="#FFFFFF"))->grafico
+  print(grafico)
+  
+})
+dev.off()
+
+#grafico per gapYears
+brewer.pal(n=9,name="YlOrRd")[4:9]->colori
+colori[as.integer(cut(abs(risultato2$gapYears),breaks = seq(-0.5,GAP_ANNI,0.5)))]->risultato2$colori
+cairo_pdf("gapYears_breakpoints_climatol_acmant.pdf",width=12,height=8,pointsize = 5)
+par(family="Lato")
+order(abs(risultato2$gapYears))->ordine
+dotchart(x=abs(risultato2$gapYears[ordine]),labels = risultato2$Code[ordine],lcolor="gainsboro",bg = risultato2$colori[ordine],color=NULL)
+dev.off()
